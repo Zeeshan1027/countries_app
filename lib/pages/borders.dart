25999d04d6cb4e21bc11bb3a7d5759029e62510a
deathd0 <dhruvarora26@gmail.com>
2021-08-29 16:12:46 +0530

    Zeeshan gandu

--- lib/pages/borders.dart
+++ lib/pages/borders.dart
@@ -1,3 +1,4 @@
+import 'package:countries/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,47 +13,89 @@ class Borders extends StatefulWidget {
 class _BordersState extends State<Borders> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                // homeFunction();
-              },
-              child: ListTile(
-                title: Text("Home"),
-                trailing: Icon(Icons.arrow_forward),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return MainPage();
+                      },
+                    ),
+                  );
+                },
+                child: ListTile(
+                  title: Text("Home"),
+                  trailing: Icon(Icons.home),
+                ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                SystemNavigator.pop();
-              },
-              child: ListTile(
-                title: Text("Exit"),
-                trailing: Icon(Icons.arrow_forward),
+              GestureDetector(
+                onTap: () {
+                  SystemNavigator.pop();
+                },
+                child: ListTile(
+                  title: Text("Exit"),
+                  trailing: Icon(Icons.exit_to_app),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      appBar: AppBar(
-        title: Text('Countries!'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            for (int i = 0; i < widget.Border.length; i++)
-              for (int j = 0; j < widget.Countries.length; j++)
-                widget.Border[i] == widget.Countries[j]["cioc"]
-                    ? Container(
-                        child: Text("${widget.Countries[j]["name"]}"),
-                      )
-                    : Container(),
-          ],
+        appBar: AppBar(
+          title: Text('Countries!'),
+          backgroundColor: Colors.cyan,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              for (int i = 0; i < widget.Border.length; i++)
+                for (int j = 0; j < widget.Countries.length; j++)
+                  widget.Border[i] == widget.Countries[j]["cioc"]
+                      ? Container(
+                          child: Text(
+                            "${widget.Countries[j]["name"]}\n-------------------'",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        )
+                      : Container(),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _onWillPop() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Confirm Exit?',
+                style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+            content: new Text(
+                'Are you sure you want to exit the app? Tap \'Yes\' to exit \'No\' to cancel.'),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () {
+                  // this line exits the app.
+                  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                },
+                child: new Text('Yes', style: new TextStyle(fontSize: 18.0)),
+              ),
+              new FlatButton(
+                onPressed: () =>
+                    Navigator.pop(context), // this line dismisses the dialog
+                child: new Text('No', style: new TextStyle(fontSize: 18.0)),
+              )
+            ],
+          ),
+        ) ??
+        false;
   }
 }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -35,48 +35,49 @@ class _MainPageState extends State<MainPage> {
     setState(() {
       Countries = k;
       flag = true;
-      print(Countries.length);
-      print('h1');
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                // homeFunction();
-              },
-              child: ListTile(
-                title: Text("Home"),
-                trailing: Icon(Icons.arrow_forward),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("Home"),
+                  trailing: Icon(Icons.home),
+                ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                SystemNavigator.pop();
-              },
-              child: ListTile(
-                title: Text("Exit"),
-                trailing: Icon(Icons.arrow_forward),
+              GestureDetector(
+                onTap: () {
+                  SystemNavigator.pop();
+                },
+                child: ListTile(
+                  title: Text("Exit"),
+                  trailing: Icon(Icons.exit_to_app),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      appBar: AppBar(
-        title: Text('Countries!'),
-      ),
-      body: flag == false
-          ? Container()
-          : SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  children: [
-                    GestureDetector(
+        appBar: AppBar(
+          title: Text('Countries!'),
+          backgroundColor: Colors.cyan,
+        ),
+        body: flag == false
+            ? Container()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
@@ -91,10 +92,28 @@ class _MainPageState extends State<MainPage> {
                         );
                       },
                       child: Container(
-                        child: Text("Asia"),
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            )),
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                          child: Text(
+                            "Asia",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w900),
+                          ),
+                        ),
                       ),
                     ),
-                    GestureDetector(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
@@ -109,10 +128,28 @@ class _MainPageState extends State<MainPage> {
                         );
                       },
                       child: Container(
-                        child: Text("Europe"),
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            )),
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                          child: Text(
+                            "Europe",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w900),
+                          ),
+                        ),
                       ),
                     ),
-                    GestureDetector(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
@@ -127,10 +164,27 @@ class _MainPageState extends State<MainPage> {
                         );
                       },
                       child: Container(
-                        child: Text("Polar"),
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            )),
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                            child: Text(
+                          "Polar",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w900),
+                        )),
                       ),
                     ),
-                    GestureDetector(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
@@ -145,10 +199,27 @@ class _MainPageState extends State<MainPage> {
                         );
                       },
                       child: Container(
-                        child: Text("Americas"),
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            )),
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                            child: Text(
+                          "Americas",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w900),
+                        )),
                       ),
                     ),
-                    GestureDetector(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
@@ -163,10 +234,27 @@ class _MainPageState extends State<MainPage> {
                         );
                       },
                       child: Container(
-                        child: Text("Oceania"),
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            )),
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                            child: Text(
+                          "Oceania",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w900),
+                        )),
                       ),
                     ),
-                    GestureDetector(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
@@ -181,22 +269,54 @@ class _MainPageState extends State<MainPage> {
                         );
                       },
                       child: Container(
-                        child: Text("Africa"),
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            )),
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                            child: Text(
+                          "Africa",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w900),
+                        )),
                       ),
                     ),
-                    // for (int i=0; i < Countries.length-1; i++)
-                    //   GestureDetector(
-                    //     onTap: (){
-                    //       print("Zeeshan Gaandu hai");
-                    //     },
-                    //     child: Container(
-                    //       child: Text('${Countries[i]["name"]}',style: TextStyle(fontSize: 15),),
-                    //     ),
-                    //   ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
+      ),
     );
+  }
+
+  Future<bool> _onWillPop() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Confirm Exit?',
+                style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+            content: new Text(
+                'Are you sure you want to exit the app? Tap \'Yes\' to exit \'No\' to cancel.'),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () {
+                  // this line exits the app.
+                  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                },
+                child: new Text('Yes', style: new TextStyle(fontSize: 18.0)),
+              ),
+              new FlatButton(
+                onPressed: () =>
+                    Navigator.pop(context), // this line dismisses the dialog
+                child: new Text('No', style: new TextStyle(fontSize: 18.0)),
+              )
+            ],
+          ),
+        ) ??
+        false;
   }
 }
